2fe46da16d8b9bc9c27802f9c7338ffb841f75c9
Digvijay Singh <Digvijaysinghonly1@gmail.com>
2024-01-21 23:02:47 +0530

    Added cors

--- api/app.js
+++ api/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const SECRET = "SECr3t"; // This should be in an environment variable in a real application
 
