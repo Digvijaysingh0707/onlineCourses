1b9e8ed9c40c24f45fc01d58d28efc16d3a1cb81
Digvijay Singh <Digvijaysinghonly1@gmail.com>
2024-01-21 23:03:10 +0530

    Added sign up api

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,6 +1,19 @@
 import { Button, Card, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import axios from "axios"
 
-function Signup() {
+const Signup = () => {
+    const [username, setUserName] = useState()
+    const [password, setPassword] = useState()
+
+    const handleSignup = async () => {
+        const response = await axios.post("http://localhost:3000/admin/signup", { username, password }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        localStorage.setItem("token", response?.data?.token)
+    }
     return (
         <div >
             <div style={{ paddingTop: 150, marginBottom: 10, display: 'flex', justifyContent: 'center' }}>
@@ -8,11 +21,11 @@ function Signup() {
             </div>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Card variant="outlined" style={{ width: 400, padding: 20 }}>
-                    <TextField fullWidth id="outlined-basic" label="Email" type={"text"} />
+                    <TextField fullWidth id="outlined-basic" label="Email" type={"text"} onChange={e => setUserName(e.target.value)} />
                     <br />
-                    <TextField fullWidth id="outlined-basic" label="Password" type={"password "} />
+                    <TextField fullWidth id="outlined-basic" label="Password" type={"password "} onChange={e => setPassword(e.target.value)} />
                     <br />
-                    <Button size={"small"} variant="contained">Sign up</Button>
+                    <Button size={"small"} variant="contained" onClick={handleSignup}>Sign up</Button>
 
                 </Card>
             </div>
